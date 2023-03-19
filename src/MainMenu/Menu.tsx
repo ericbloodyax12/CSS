b452aef986faec75ef0eb2e2f6c039bfc8eb4c3e
Young Eric <erikbonopart12@gmail.com>
2023-03-19 19:04:11 +0300

    feat: -add mapper for li and add icon for last element

--- src/MainMenu/Menu.tsx
+++ src/MainMenu/Menu.tsx
@@ -2,22 +2,34 @@ import React from 'react';
 import './menu.css';
 
 function Menu(props:any) {
-
+const menuItemsTxt = ["home","my friends","my messages","my activity","photos"]
+    const itemsJSX = menuItemsTxt.map((el,index) => {
+        let lastElIcon =  (menuItemsTxt.length-1 === index) ? <i className="fa fa-camera" aria-hidden="true"></i> : false
+        return <li key={index} id={`item-${index}`} className={"item"}>
+            {lastElIcon} {el}
+        </li>
+    })
     return (
+        <>
             <header className={'header'} id={"h1"}>
-                <ul className='container'>
-                    <li className={'item'}>home</li>
-                    <li className={'item'}>my friends</li>
-                    <li className={'item'}>my messages</li>
-                    <li className={'item'}>my activity</li>
-                    <li className={'item'}>photos</li>
+                <ul className='menu-container'>
+                    {itemsJSX}
                 </ul>
+                {/*<ul className='container2'>*/}
+                {/*    <li className={'item2'} id={"home"}>home</li>*/}
+                {/*    <li className={'item2'}>my friends</li>*/}
+                {/*    <li className={'item2'}>my messages</li>*/}
+                {/*    <li className={'item2'}>my activity</li>*/}
+                {/*    <li className={'item2'}>photos</li>*/}
+                {/*</ul>*/}
                 <div className="invisible">
+                    <p>Lorem ipsum dolor sit amet.</p>
                     <div className="mobile-menu"></div>
                     <div className="mobile-menu"></div>
                     <div className="mobile-menu"></div>
                 </div>
             </header>
+        </>
     );
 }
 
